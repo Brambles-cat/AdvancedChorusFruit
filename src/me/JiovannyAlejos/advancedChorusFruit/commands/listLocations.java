34ddbc93c9c79c36775e562e49fd166b74622e0f
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2021-08-05 19:14:36 -0700

    listLocations command completed

--- src/me/JiovannyAlejos/advancedChorusFruit/commands/listLocations.java
+++ src/me/JiovannyAlejos/advancedChorusFruit/commands/listLocations.java
@@ -1,11 +1,20 @@
 package me.JiovannyAlejos.advancedChorusFruit.commands;
 
-import org.bukkit.GameMode;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.google.gson.Gson;
+
+import me.JiovannyAlejos.advancedChorusFruit.CoordinateData;
 import me.JiovannyAlejos.advancedChorusFruit.Main;
 
 public class listLocations implements CommandExecutor {
@@ -19,11 +28,26 @@ public class listLocations implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(!(sender instanceof Player)) {
 			sender.sendMessage("Only players can use commands for this plugin");
-			return true; // returning true means
+			return true;
+		}
+		Gson gson = new Gson();
+		Reader reader = null;
+		try {
+			reader = Files.newBufferedReader(Paths.get("C:\\Users\\gaela\\OneDrive\\Documents\\advanedChorusFruitPlugin\\src\\me\\JiovannyAlejos\\advancedChorusFruit\\tpData.json"));
+		} catch (IOException e) {e.printStackTrace();}
+		CoordinateData data = gson.fromJson(reader, CoordinateData.class);
+		String coordList = "List of warp locations\n";
+		if(data.coordinates.size() == 0) {
+			coordList = "No warp locations set";
+		} else {
+			ArrayList<String> names = data.locNames;
+			for(int i = 0; i < data.coordinates.size(); i++) {
+				String[] coordinates = data.coordinates.get(i).split(Pattern.quote("|"));
+				coordList += "\n" + names.get(i) + ", " + coordinates[0] + ", " + coordinates[1] + ", " + coordinates[2];
+			}
 		}
 		Player p = (Player) sender;
-		p.setGameMode(GameMode.CREATIVE);
-		p.sendMessage("test");
+		p.sendMessage(coordList);
 		return false;
 	}
 }

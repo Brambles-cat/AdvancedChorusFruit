e6c16bd6b821ebd0e8f8250fc7c248a72e612e81
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2021-08-09 19:00:27 -0700

    more concise + took away some unnecessary lines

--- src/me/JiovannyAlejos/advancedChorusFruit/listeners/PlayerConsumeItem.java
+++ src/me/JiovannyAlejos/advancedChorusFruit/listeners/PlayerConsumeItem.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.regex.Pattern;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -20,25 +21,19 @@ public class PlayerConsumeItem implements Listener {
 	@EventHandler
 	public void onPlayerItemConsume(PlayerItemConsumeEvent event) {
 		Player player = event.getPlayer();
-		System.out.println(event.getItem().getType());
 		String itemDisplayName = event.getItem().getItemMeta().getDisplayName();
 		if(event.getItem().getType() == Material.GOLDEN_APPLE && itemDisplayName.substring(0, 5).equals("warp ")) {
 			Gson gson = new Gson();
 			try {
 				Reader reader = Files.newBufferedReader(Paths.get("C:\\Users\\gaela\\OneDrive\\Documents\\advanedChorusFruitPlugin\\src\\me\\JiovannyAlejos\\advancedChorusFruit\\tpData.json"));
 				CoordinateData data = gson.fromJson(reader, CoordinateData.class);
-				System.out.println(data.locNames.contains(itemDisplayName.substring(5)));
 				if(data.locNames.contains(itemDisplayName.substring(5))) {
-					String[] coords = data.coordinates.get(data.locNames.indexOf(itemDisplayName.substring(5))).split("|");
-					try {
-						System.out.println(coords.toString());
-					} catch(Error err) {
-						System.out.println(coords + "hi");
-					}
+					String[] coords = data.coordinates.get(data.locNames.indexOf(itemDisplayName.substring(5))).split(Pattern.quote("|"));
 					player.teleport(new Location(player.getWorld(), Double.parseDouble(coords[0]), Double.parseDouble(coords[1]), Double.parseDouble(coords[2])));
 					reader.close();
 				}
-			} catch (IOException e) {e.printStackTrace();}
+			} catch (IOException e) {e.printStackTrace(); System.out.println("no gud eat");}
 		}
 	}
 }
+

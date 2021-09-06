aa4db5bf84c2d600389f3b93c56c4ccdce9198e4
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2021-09-05 22:40:23 -0700

    Update AdvancedChorusFruit.java

--- src/main/java/me/jiovannyalejos/advancedchorusfruit/AdvancedChorusFruit.java
+++ src/main/java/me/jiovannyalejos/advancedchorusfruit/AdvancedChorusFruit.java
@@ -1,16 +1,46 @@
 package me.jiovannyalejos.advancedchorusfruit;
 
+import com.google.gson.Gson;
 import me.jiovannyalejos.advancedchorusfruit.commands.ListLocations;
 import me.jiovannyalejos.advancedchorusfruit.commands.RemoveLocation;
 import me.jiovannyalejos.advancedchorusfruit.listeners.EntityBurn;
 import me.jiovannyalejos.advancedchorusfruit.listeners.PlayerTeleport;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 public final class AdvancedChorusFruit extends JavaPlugin {
+    public static String dataPath;
+    static String dataFolderPath;
+    static Gson gson = new Gson();
+    static Reader reader;
+
+    public static CoordinateData getData() {
+        try {
+            if(!(Files.exists(Paths.get(dataPath)))) {
+                Files.createDirectory(Paths.get(dataFolderPath));
+                Files.createFile(Paths.get(dataPath));
+                Writer writer = new FileWriter(AdvancedChorusFruit.dataPath);
+                CoordinateData data = new CoordinateData(new ArrayList<>(), new ArrayList<>());
+                gson.toJson(data, writer);
+                writer.close();
+            }
+            reader = Files.newBufferedReader(Paths.get(dataPath));
+        } catch (IOException ignored) { }
+        return gson.fromJson(reader, CoordinateData.class);
+    }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        dataFolderPath = getDataFolder().getPath();
+        dataPath = dataFolderPath + "/TeleportData.json";
+        AdvancedChorusFruit.getData();
         getCommand("listlocations").setExecutor(new ListLocations(this));
         getCommand("removelocation").setExecutor(new RemoveLocation(this));
         getServer().getPluginManager().registerEvents(new PlayerTeleport(), this);
@@ -20,6 +50,5 @@ public final class AdvancedChorusFruit extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }

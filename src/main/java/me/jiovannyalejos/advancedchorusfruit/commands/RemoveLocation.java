3b1244d2afa7ea63cb0fbacdab625d3623960844
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2021-09-06 19:46:57 -0700

    Removes location if you are in its dimension
    Just in case different dimensions share the same warp location names

--- src/main/java/me/jiovannyalejos/advancedchorusfruit/commands/RemoveLocation.java
+++ src/main/java/me/jiovannyalejos/advancedchorusfruit/commands/RemoveLocation.java
@@ -1,12 +1,14 @@
 package me.jiovannyalejos.advancedchorusfruit.commands;
 
-import com.google.common.base.FinalizableReferenceQueue;
 import com.google.gson.Gson;
 import me.jiovannyalejos.advancedchorusfruit.AdvancedChorusFruit;
 import me.jiovannyalejos.advancedchorusfruit.CoordinateData;
+import me.jiovannyalejos.advancedchorusfruit.Dimension;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -25,17 +27,19 @@ public class RemoveLocation implements CommandExecutor {
         if(args.length != 0) {
             Gson gson = new Gson();
             CoordinateData data = AdvancedChorusFruit.getData();
+            World.Environment env = ((Player) sender).getWorld().getEnvironment();
+            Dimension dimData = CoordinateData.getDimData(env, data);
             String argLocName = "";
             for(String word : args) {
                 argLocName += word + " ";
             }
-            int index = data.locNames.indexOf(argLocName.trim());
-            if(data.locNames.contains(argLocName.trim())) {
-                data.coordinates.remove(index);
-                data.locNames.remove(index);
+            int index = dimData.locNames.indexOf(argLocName.trim());
+            if(dimData.locNames.contains(argLocName.trim())) {
+                dimData.coordinates.remove(index);
+                dimData.locNames.remove(index);
                 try {
                     Writer writer = new FileWriter(AdvancedChorusFruit.dataPath);
-                    gson.toJson(data, writer);
+                    gson.toJson(CoordinateData.assignData(env, data, dimData), writer);
                     writer.close();
                     sender.sendMessage("Successfully removed warp location '" + argLocName.trim() + "'");
                 } catch (IOException e) {e.printStackTrace();}

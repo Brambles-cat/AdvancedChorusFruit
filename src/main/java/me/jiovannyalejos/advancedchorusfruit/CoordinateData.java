4970c40f58fd566424503bd1f541e4f2c5db0416
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2021-09-06 19:56:08 -0700

    Now stores data for different dimensions

--- src/main/java/me/jiovannyalejos/advancedchorusfruit/CoordinateData.java
+++ src/main/java/me/jiovannyalejos/advancedchorusfruit/CoordinateData.java
@@ -1,12 +1,35 @@
 package me.jiovannyalejos.advancedchorusfruit;
 
+import org.bukkit.World;
+
 import java.util.ArrayList;
 
 public class CoordinateData {
-    public ArrayList<String> coordinates;
-    public ArrayList<String> locNames;
+    public Dimension nether;
+    public Dimension end;
+    public Dimension overworld;
     public CoordinateData(ArrayList<String> coordinates, ArrayList<String> locNames) {
-        this.coordinates = coordinates;
-        this.locNames = locNames;
+        this.nether = new Dimension(coordinates, locNames);
+        this.overworld = new Dimension(coordinates, locNames);
+        this.end = new Dimension(coordinates, locNames);
+    }
+    public static Dimension getDimData(World.Environment env, CoordinateData data) {
+         if(env == World.Environment.NORMAL) {
+            return data.overworld;
+        } else if(env == World.Environment.NETHER) {
+            return data.nether;
+        } else {
+            return data.end;
+        }
+    }
+    public static CoordinateData assignData(World.Environment env, CoordinateData data, Dimension dimData) {
+        if(env == World.Environment.NORMAL) {
+            data.overworld = dimData;
+        } else if(env == World.Environment.NETHER) {
+            data.nether = dimData;
+        } else {
+            data.end = dimData;
+        }
+        return data;
     }
 }

67e788c90916c697f9193af89c7d2dea276ac878
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2021-09-06 19:57:24 -0700

    Create Dimension.java

---
+++ src/main/java/me/jiovannyalejos/advancedchorusfruit/Dimension.java
@@ -0,0 +1,12 @@
+package me.jiovannyalejos.advancedchorusfruit;
+
+import java.util.ArrayList;
+
+public class Dimension {
+    public ArrayList<String> coordinates;
+    public ArrayList<String> locNames;
+    public Dimension(ArrayList<String> coordinates, ArrayList<String> locNames) {
+        this.coordinates = coordinates;
+        this.locNames = locNames;
+    }
+}

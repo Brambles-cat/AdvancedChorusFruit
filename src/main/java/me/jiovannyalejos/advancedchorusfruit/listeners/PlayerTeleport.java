0aa816a34edb599bcc9696f7692e1f59604469a0
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2021-09-06 19:50:42 -0700

    Update PlayerTeleport.java

--- src/main/java/me/jiovannyalejos/advancedchorusfruit/listeners/PlayerTeleport.java
+++ src/main/java/me/jiovannyalejos/advancedchorusfruit/listeners/PlayerTeleport.java
@@ -2,24 +2,26 @@ package me.jiovannyalejos.advancedchorusfruit.listeners;
 
 import me.jiovannyalejos.advancedchorusfruit.AdvancedChorusFruit;
 import me.jiovannyalejos.advancedchorusfruit.CoordinateData;
+import me.jiovannyalejos.advancedchorusfruit.Dimension;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
-import java.io.IOException;
 import java.util.regex.Pattern;
 
 public class PlayerTeleport implements Listener {
     @EventHandler
-    public void onPlayerTeleport(PlayerTeleportEvent event) throws IOException {
+    public void onPlayerTeleport(PlayerTeleportEvent event) {
+        if(!(event.getCause() == PlayerTeleportEvent.TeleportCause.CHORUS_FRUIT)) return;
         Player player = event.getPlayer();
         String itemDisplayName = player.getItemInUse().getItemMeta().getDisplayName();
-        if((event.getCause() == PlayerTeleportEvent.TeleportCause.CHORUS_FRUIT) && itemDisplayName.startsWith("warp ")) {
+        if(itemDisplayName.startsWith("warp ")) {
             CoordinateData data = AdvancedChorusFruit.getData();
-            if(data.locNames.contains(itemDisplayName.substring(5))) {
-                String[] coords = data.coordinates.get(data.locNames.indexOf(itemDisplayName.substring(5))).split(Pattern.quote("|"));
+            Dimension dimData = CoordinateData.getDimData(event.getPlayer().getWorld().getEnvironment(), data);
+            if(dimData.locNames.contains(itemDisplayName.substring(5))) {
+                String[] coords = dimData.coordinates.get(dimData.locNames.indexOf(itemDisplayName.substring(5))).split(Pattern.quote("|"));
                 event.setTo(new Location(player.getWorld(), Double.parseDouble(coords[0]), Double.parseDouble(coords[1]), Double.parseDouble(coords[2])));
             }
         }

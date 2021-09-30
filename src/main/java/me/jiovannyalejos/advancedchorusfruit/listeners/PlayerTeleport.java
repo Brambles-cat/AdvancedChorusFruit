3582e1353943119ee8e91c400c855258742aacd1
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2021-09-29 19:00:52 -0700

    Update PlayerTeleport.java

--- src/main/java/me/jiovannyalejos/advancedchorusfruit/listeners/PlayerTeleport.java
+++ src/main/java/me/jiovannyalejos/advancedchorusfruit/listeners/PlayerTeleport.java
@@ -1,8 +1,6 @@
 package me.jiovannyalejos.advancedchorusfruit.listeners;
 
 import me.jiovannyalejos.advancedchorusfruit.AdvancedChorusFruit;
-import me.jiovannyalejos.advancedchorusfruit.CoordinateData;
-import me.jiovannyalejos.advancedchorusfruit.Dimension;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,6 +8,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class PlayerTeleport implements Listener {
@@ -19,10 +18,9 @@ public class PlayerTeleport implements Listener {
         Player player = event.getPlayer();
         ItemMeta meta = player.getItemInUse().getItemMeta();
         if(meta.getLore() != null && meta.getLore().contains("warp")) {
-            CoordinateData data = AdvancedChorusFruit.getData();
-            Dimension dimData = CoordinateData.getDimData(event.getPlayer().getWorld().getEnvironment(), data);
-            if(dimData.locNames.contains(meta.getDisplayName())) {
-                String[] coords = dimData.coordinates.get(dimData.locNames.indexOf(meta.getDisplayName())).split(Pattern.quote("|"));
+            Map<String, String> data = AdvancedChorusFruit.getData().dimensions.get(player.getWorld().getEnvironment());
+            if(data.containsKey(meta.getDisplayName())) {
+                String[] coords = data.get(meta.getDisplayName()).split(Pattern.quote("|"));
                 event.setTo(new Location(player.getWorld(), Double.parseDouble(coords[0]), Double.parseDouble(coords[1]), Double.parseDouble(coords[2])));
                 event.getTo().setDirection(player.getLocation().getDirection());
             }

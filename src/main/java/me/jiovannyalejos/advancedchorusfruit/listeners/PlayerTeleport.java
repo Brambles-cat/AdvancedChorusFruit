785f284c6a86584afbd5b8a8b09ff8fb7abf6bec
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2023-02-27 16:17:46 -0800

    No longer gets item meta of held item

--- src/main/java/me/jiovannyalejos/advancedchorusfruit/listeners/PlayerTeleport.java
+++ src/main/java/me/jiovannyalejos/advancedchorusfruit/listeners/PlayerTeleport.java
@@ -1,6 +1,7 @@
 package me.jiovannyalejos.advancedchorusfruit.listeners;
 
 import java.util.Map;
+import java.util.UUID;
 import java.util.regex.Pattern;
 import me.jiovannyalejos.advancedchorusfruit.AdvancedChorusFruit;
 import org.bukkit.Location;
@@ -9,20 +10,21 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class PlayerTeleport implements Listener {
     @EventHandler
     public void onPlayerTeleport(PlayerTeleportEvent event) {
         if (event.getCause() == TeleportCause.CHORUS_FRUIT) {
             Player player = event.getPlayer();
-            ItemMeta meta = player.getItemInUse().getItemMeta();
-            if (meta.getLore() != null && meta.getLore().contains("warp")) {
+            UUID playerId = player.getUniqueId();
+            if (ItemConsume.consumers.containsKey(playerId)) {
                 Map<String, String> data = AdvancedChorusFruit.getData().dimensions.get(player.getWorld().getEnvironment());
-                if (data.containsKey(meta.getDisplayName())) {
-                    String[] coords = data.get(meta.getDisplayName()).split(Pattern.quote("|"));
+                String warpLocation = ItemConsume.consumers.get(playerId);
+                if (data.containsKey(warpLocation)) {
+                    String[] coords = data.get(warpLocation).split(Pattern.quote("|"));
                     event.setTo(new Location(player.getWorld(), Double.parseDouble(coords[0]), Double.parseDouble(coords[1]), Double.parseDouble(coords[2])));
                     event.getTo().setDirection(player.getLocation().getDirection());
+                    ItemConsume.consumers.remove(playerId);
                 }
             }
         }

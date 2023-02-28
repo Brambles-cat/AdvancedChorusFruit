f7d28fe218083c8d003a3e3fc324a92b4686f481
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2023-02-27 16:17:14 -0800

    Renamed

---
+++ src/main/java/me/jiovannyalejos/advancedchorusfruit/commands/ListWarps.java
@@ -0,0 +1,64 @@
+package me.jiovannyalejos.advancedchorusfruit.commands;
+
+import java.util.Map;
+import java.util.regex.Pattern;
+import me.jiovannyalejos.advancedchorusfruit.AdvancedChorusFruit;
+import me.jiovannyalejos.advancedchorusfruit.Data;
+import org.bukkit.World.Environment;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ListWarps implements CommandExecutor {
+    private AdvancedChorusFruit plugin;
+
+    public ListWarps(AdvancedChorusFruit plugin) {
+        this.plugin = plugin;
+        plugin.getCommand("listwarps").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Environment env;
+        Data data = AdvancedChorusFruit.getData();
+        if (sender instanceof Player) {
+            env = ((Player)sender).getWorld().getEnvironment();
+            if(!data.permissions.containsKey(sender.getName()) || !data.permissions.get(sender.getName()).contains("listwarps")) return true;
+        } else {
+            if (args.length == 0) {
+                sender.sendMessage("Required dimension as an argument, use /listlocations Overworld/Nether/End");
+                return false;
+            }
+
+            String dimension = args[0].toLowerCase();
+            switch(dimension) {
+                case "overworld":
+                    env = Environment.NORMAL;
+                    break;
+                case "nether":
+                    env = Environment.NETHER;
+                    break;
+                case "end":
+                    env = Environment.THE_END;
+                    break;
+                default:
+                    sender.sendMessage("This dimension doesn't exist, choose \"Overworld\", \"Nether\", or \"End\"");
+                    return false;
+            }
+        }
+        Map<String, String> warpPoints = data.dimensions.get(env);
+        StringBuilder coordList = new StringBuilder("No warp locations set");
+        if (!warpPoints.isEmpty()) {
+            coordList = new StringBuilder("List of warp locations in the " + Data.format(env) + "\n");
+
+            for(String key : warpPoints.keySet()) {
+                String[] coordinates = warpPoints.get(key).split(Pattern.quote("|"));
+                coordList.append("\n").append(key).append(": ").append(coordinates[0]).append(", ").append(coordinates[1]).append(", ").append(coordinates[2]);
+            }
+        }
+
+        sender.sendMessage(coordList.toString());
+        return true;
+    }
+}

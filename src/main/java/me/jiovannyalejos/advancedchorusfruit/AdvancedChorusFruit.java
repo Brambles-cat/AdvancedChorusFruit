5f9cf52dba01cd545be9222e67785e11c82be769
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2023-03-01 21:33:36 -0800

    Moved data related to PluginData and registered cmds

--- src/main/java/me/jiovannyalejos/advancedchorusfruit/AdvancedChorusFruit.java
+++ src/main/java/me/jiovannyalejos/advancedchorusfruit/AdvancedChorusFruit.java
@@ -1,78 +1,29 @@
 package me.jiovannyalejos.advancedchorusfruit;
 
-import com.google.gson.Gson;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
-import java.io.Writer;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Map;
+import me.jiovannyalejos.advancedchorusfruit.commands.Permission;
 import me.jiovannyalejos.advancedchorusfruit.commands.ListWarps;
-import me.jiovannyalejos.advancedchorusfruit.commands.RemoveLocation;
-import me.jiovannyalejos.advancedchorusfruit.commands.SetOpExclusive;
+import me.jiovannyalejos.advancedchorusfruit.commands.RemoveWarp;
 import me.jiovannyalejos.advancedchorusfruit.listeners.AnvilPrepareListener;
 import me.jiovannyalejos.advancedchorusfruit.listeners.EntityBurn;
 import me.jiovannyalejos.advancedchorusfruit.listeners.ItemConsume;
 import me.jiovannyalejos.advancedchorusfruit.listeners.PlayerTeleport;
-import org.bukkit.World.Environment;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AdvancedChorusFruit extends JavaPlugin {
-    public static String dataPath;
-    static String dataFolderPath;
-    static Gson gson = new Gson();
-    static Reader reader;
-
-    public static Data getData() {
-        try {
-            if (!Files.exists(Paths.get(dataPath))) {
-                Files.createDirectory(Paths.get(dataFolderPath));
-                Files.createFile(Paths.get(dataPath));
-                Writer writer = new FileWriter(dataPath);
-                Data data = new Data();
-                gson.toJson(data, writer);
-                writer.close();
-            }
-            reader = Files.newBufferedReader(Paths.get(dataPath));
-        } catch (IOException exception) {exception.printStackTrace();}
-        Data data = gson.fromJson(reader, Data.class);
-        return data;
-    }
-
-    public static void writeData(Environment env, Map<String, String> data, Data original) {
-        original.dimensions.replace(env, data);
-
-        try {
-            Writer writer = new FileWriter(dataPath);
-            gson.toJson(original, writer);
-            writer.close();
-        } catch (IOException exception) {exception.printStackTrace();}
-    }
-
-    public static void writeData(Data data) {
-        try {
-            Writer writer = new FileWriter(dataPath);
-            gson.toJson(data, writer);
-            writer.close();
-        } catch (IOException var2) {
-            var2.printStackTrace();
-        }
-    }
 
     public void onEnable() {
-        dataFolderPath = this.getDataFolder().getPath();
-        dataPath = dataFolderPath + "/TeleportData.json";
-        this.getCommand("listwarps").setExecutor(new ListWarps(this));
-        this.getCommand("setopexclusive").setExecutor(new SetOpExclusive(this));
-        this.getCommand("removelocation").setExecutor(new RemoveLocation(this));
+        PluginData.dataFolderPath = this.getDataFolder().getPath();
+        PluginData.dataPath = PluginData.dataFolderPath + "/PluginData.json";
+        getCommand("listwarps").setExecutor(new ListWarps());
+        getCommand("removewarp").setExecutor(new RemoveWarp());
+        getCommand("permission").setExecutor(new Permission());
         this.getServer().getPluginManager().registerEvents(new AnvilPrepareListener(), this);
         this.getServer().getPluginManager().registerEvents(new ItemConsume(), this);
         this.getServer().getPluginManager().registerEvents(new PlayerTeleport(), this);
         this.getServer().getPluginManager().registerEvents(new EntityBurn(), this);
+        PluginData.playerPermissions = PluginData.getData().permissions;
         this.getServer().getConsoleSender().sendMessage("Advanced Chorus Fruit plugin ready");
     }
 
-    public void onDisable() {
-    }
+    public void onDisable() {}
 }
